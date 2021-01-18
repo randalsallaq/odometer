ce09e52f6aad772855d6a77e4387f8645c7694ff
rand alsallaq <randalsallaq97@gmail.com>
2021-01-18 20:08:43 +0200

    counter edits

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props) {
+  return (
+
+    <header className = 'header' > {props.text} </header>
+  );
+
+}
+
+function Footer(props) {
+  return (
+    <footer className = 'header'>{props.tradeMark}</footer>
+  );
+}
+
+
+class Odometer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+
+
+    };
+
+  }
+
+  increaseCounter(value) {
+    if (this.state.counter + value > 9999){
+      this.state.counter = this.state.counter + value - 10001
+
+    }
+    this.setState({ counter: this.state.counter + value });
+
+  }
+
+  render(){
+    return(
+<>
+<button className = 'button' onClick={()=> this.increaseCounter(1)}>+1</button>
+<button className = 'button' onClick={()=> this.increaseCounter(10)}>+10</button>
+<button className = 'button' onClick={()=> this.increaseCounter(100)}>+100</button>
+<button className = 'button' onClick={()=> this.increaseCounter(1000)}>+1000</button>
+<h1 className = 'counter'>{String(this.state.counter).padStart(4,0)}</h1>
+</>
+    );
+  }
+}
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header text='Hello from the other side' />
+      <Odometer />
+      <Footer tradeMark='RAND' />
+    </>
   );
 }
 
